df46e599675ce9c62d2cb953d71568c18947493a
Андрюша <galuza_a@list.ru>
2023-03-13 00:03:04 +0300

    рефакторинг

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,104 +1,52 @@
 // const { connection, searchBase } = require('./DataBaseControlle')
 const path = require('path')
-const mysql = require("mysql2");
 const { session } = require("../app");
 const { emit } = require('process');
-var connection = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    database: "PixelBattle",
-    password: "",
-    port: "3306"
-});
+
+const { dataBaseControlle } = require(
+    "./DataBaseControlle"
+)
 
 module.exports.login = function(req, res) {
-
-
     if (!req.body) return res.sendStatus(400);
-    // console.log(req.body);
-    // var aut = searchBase(req.body.user, req.body.password)
     var user = req.body.user
     var password = req.body.password
-    var sql = `SELECT * from \`users\` WHERE login = \"${user}\" and pasworld=\"${password}\"; `
-
-    // UPDATE `users` SET `session`='37KP4SZdgKClPro0arsTd35_gZewCO71' WHERE id=0
-    connection.query(sql, function(err, results) {
-        if (err) {
-            console.log(err);
-            res.writeHead(301, {
-                Location: `../../`
-            }).end();
-            return;
-
-        } else {
-
-
-            if (results[0] === undefined) {
-
-                res.writeHead(301, {
-                    Location: `../../`
-                }).end();
-
-                return;
-
-            } else {
-
-                sql = `UPDATE \`users\` SET \`session\`='${req.session.id}' WHERE id=${results[0].id}`
-                res.writeHead(301, {
-                    Location: `../../`
-                }).end();
-                connection.query(sql, function(err, results) {
-
-                })
-
-                return;
-            }
-
-
-        }
-
-    });
-
-
+    var sessionId = req.session.id
+    if (!dataBaseControlle.login(user, password, sessionId)) {
+        //код ошибки клиенту
+        return
+    } else {
+        res.writeHead(301, {
+            Location: `../../`
+        }).end();
+    }
 
 }
 
 module.exports.register = function(req, res) {
 
-    console.log("register")
     if (!req.body) return res.sendStatus(400);
-    // console.log(req.body);
-    // var aut = searchBase(req.body.user, req.body.password)
     var user = req.body.user
     var password = req.body.password
-        // if (password.length < 5) {
-        //     res.writeHead(301, {
-        //         Location: `../../`
-        //     }).end();
-        //     return
-        // }
-        // var sql = `SELECT * FROM \`users\` WHERE login="${user}";` 
-
-    var sql = `INSERT INTO \`users\`( \`session\`, \`login\`, \`pasworld\` ) VALUES ('${req.session.id}','${user}','${password}')`
-        // `INSERT INTO \`gorod\` ( \`name\`, \`area\`, \`pupulation\`) VALUES ('${name}','${area}','${pupulation}')`
-
-    connection.query(sql, function(err, results) {
-        if (err) console.log(err)
-    })
+    var sessionId = req.session.id
+    if (!dataBaseControlle.register(user, password, sessionId)) {
+        //код ошибки клиенту
+        return
+    }
     res.writeHead(301, {
         Location: `../../`
     }).end();
 
 }
 
-module.exports.exit = function(req, res) {
-    var ses = req.session.id
-    sql = `UPDATE \`users\` SET session='0' WHERE  session=0`
 
-    connection.query(sql, function(err, results) {
-        if (err) console.log(err)
-        console.log("exit", results)
-    })
+module.exports.exit = function(req, res) {
+    if (!req.body) return res.sendStatus(400);
+    var ses = req.session.id
+    if (!dataBaseControlle.SessionExit(ses)) {
+        //код ошибки клиенту
+        return
+    }
     res.writeHead(301, {
         Location: `../../`
     }).end();
